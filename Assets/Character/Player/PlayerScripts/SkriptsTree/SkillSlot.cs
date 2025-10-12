1f9153f48d39624593e3c5c0b392baedac9be788
Dremted <Dremted@gmail.com>
2025-10-13 02:17:08 +0500

    SkillTree

---
+++ Assets/Character/Player/PlayerScripts/SkriptsTree/SkillSlot.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillSlot : MonoBehaviour
+{
+    public SkillSO skillSO;
+
+    public Image skillIcon;
+
+    private void OnValidate()
+    {
+        if (skillSO != null)
+            skillIcon.sprite = skillSO.skillIcon; 
+    }
+}
